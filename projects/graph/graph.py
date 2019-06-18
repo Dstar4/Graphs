d7f62a6e524a4f7216bf1b26fcc9073963abf76c
Daniel <danielstarling4@gmail.com>
2019-06-18 12:08:24 -0700

    day 2 start

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -230,3 +230,28 @@ if __name__ == '__main__':
         [1, 2, 4, 7, 6]
     '''
     print(graph.dfs(1, 6))
+
+"""
+Create an empty set to store visited nodes
+        # Create an empty Queue and enqueue the starting vertex
+        # While the queue is not empty...
+            # Dequeue the first vertex
+            # If that vertex has not been visited...
+                # Mark it as visited
+                # Then add all of its neighbors to the back of the queue
+
+BFS
+# Create an empty set to store visited nodes
+        # Create an empty Queue and enqueue A PATH TO the starting vertex
+        # While the queue is not empty...
+            # Dequeue the first PATH
+            # GRAB THE VERTEX FROM THE END OF THE PATH
+            # IF VERTEX = TARGET, RETURN PATH
+            # If that vertex has not been visited...
+                # Mark it as visited
+                # Then add A PATH TO all of its neighbors to the back of the queue
+                    # Copy the path
+                    # Append neighbor to the back of the copy
+                    # Enqueue copy
+
+"""
